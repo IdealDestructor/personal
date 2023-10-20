ba2b5fd5a642077a71f0d72d19ec2149b683d8c6
wdq <wangdq@glodon.com>
2023-10-20 14:30:19 +0800

    Update Footer.tsx

--- pages/index/_components/Footer.tsx
+++ pages/index/_components/Footer.tsx
@@ -1,9 +1,9 @@
 export default () => {
-  return (
-    <footer mt-12>
-      <p text-sm op-40>
-        Diu © 2022
-      </p>
-    </footer>
-  )
+    return (
+        <footer mt-12>
+            <p text-sm op-40>
+                Francis © 2023
+            </p>
+        </footer>
+    )
 }
